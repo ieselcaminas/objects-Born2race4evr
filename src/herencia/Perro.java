d3b65b3758d3cb5b4632b64e21faa262a7eaab1a
julpermaz <julpermaz@alu.edu.gva.es>
2025-01-23 11:43:56 +0100

    .

---
+++ src/herencia/Perro.java
@@ -0,0 +1,24 @@
+package herencia;
+public class Perro extends Mamifero{
+
+    Perro(String nombre){
+        super(nombre);
+    }
+
+    public void traerZapatillas(){
+        System.out.println("Toma amo tus zapatillas");
+    }
+
+    @Override
+    public void comunicarse()
+    {
+
+        System.out.println("guau");
+
+    }
+    @Override
+    public String toString()
+    {
+        return  super.toString() + " - Perro "  ;
+    }
+}

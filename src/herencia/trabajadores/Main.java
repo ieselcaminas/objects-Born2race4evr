1f029d83e5635a4a1f9db9979f3819c3121346bc
julpermaz <julpermaz@alu.edu.gva.es>
2025-02-20 12:10:22 +0100

    .

--- src/herencia/trabajadores/Main.java
+++ src/herencia/trabajadores/Main.java
@@ -2,6 +2,7 @@ package herencia.trabajadores;
 
 public class Main {
     public static void main(String[] args) {
-
+        Tecnico t = new Tecnico("Pepe");
+        System.out.println(t);
     }
 }
